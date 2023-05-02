156380d427845e075b2ce4de98b8cd09b0ab4655
Tony Alosius <104986715+TonyAlosius@users.noreply.github.com>
2023-05-02 17:58:33 +0530

    Path has been modified

--- Multi_disease_prediction_web_app.py
+++ Multi_disease_prediction_web_app.py
@@ -15,9 +15,9 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/tonya/ML_Deploy/Prediction_Model/models/diabetes_model.sav', 'rb'))
-heart_disease_model = pickle.load(open('C:/Users/tonya/ML_Deploy/Prediction_Model/models/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('C:/Users/tonya/ML_Deploy/Prediction_Model/models/parkinsons_model.sav', 'rb'))
+diabetes_model = pickle.load(open('streamlitapp/models/diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('streamlitapp/models/dheart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('streamlitapp/models/parkinsons_model.sav', 'rb'))
 
 
 
